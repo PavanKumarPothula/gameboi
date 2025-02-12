56c81b041a1536e8f189fb8d6a7a6afcd4075ab0
Pavan Kumar Pothula <ppavanrockz@gmail.com>
2025-02-12 21:48:50 +0530

    RTOS integration works but the Pin configs needs to be defined in the platformio.ini to get TFT display working

--- include/main.hpp
+++ include/main.hpp
@@ -48,3 +48,44 @@
 #define IRQ_PIN 9
 
 #define SERIAL_DEBUG
+
+
+typedef struct
+{
+  int16_t x_pos = X_REF;
+  int16_t y_pos = X_REF;
+  bool button_A = false;
+  bool button_B = false;
+  bool button_X = false;
+  bool button_Y = false;
+  bool button_SRT = false;
+  bool button_SEL = false;
+} gamepad_input;
+
+bool operator==(const gamepad_input& lhs, const gamepad_input& rhs)
+{
+    return ( 
+                (lhs.button_A == rhs.button_A) 
+                || (lhs.button_B == rhs.button_B) 
+                || (lhs.button_X == rhs.button_X) 
+                || (lhs.button_Y == rhs.button_Y) 
+                || (lhs.button_SEL == rhs.button_SEL) 
+                || (lhs.button_SRT == rhs.button_SRT)
+                || (lhs.x_pos == rhs.x_pos) 
+                || (lhs.y_pos == rhs.y_pos)
+    );  
+}
+
+String prettify (const gamepad_input x)
+{
+  return (
+              " X: " + String(x.x_pos) 
+          + "\n Y: " + String(x.y_pos) 
+          + "\n Button A:   " +String(x.button_A)  
+          + "\n Button B:   " + String(x.button_B) 
+          + "\n Button Y:   " + String(x.button_X) 
+          + "\n Button X:   " + String(x.button_Y) 
+          + "\n Button SEL: " + String(x.button_SEL) 
+          + "\n Button SRT: " + String(x.button_SRT)
+  );
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,6 @@ TFT_eSprite spr = TFT_eSprite(&tft);
 // Adafruit Seesaw inits
 Adafruit_seesaw ss;
 
-typedef struct
-{
-  int16_t x_pos = X_REF;
-  int16_t y_pos = X_REF;
-  bool button_A = false;
-  bool button_B = false;
-  bool button_X = false;
-  bool button_Y = false;
-  bool button_SRT = false;
-  bool button_SEL = false;
-} gamepad_input;
-
 static gamepad_input last_input;
 
 xQueueHandle graphicUpdateQueue;
@@ -28,26 +16,34 @@ void TFT_write(void *pvParameters)
 {
 
 #ifdef SERIAL_DEBUG
-  Serial.println("Entering TFT_write task \n");
+  Serial.println("#1::Entering TFT_write task \n");
 #endif
 
   gamepad_input gp_read;
-  if ( !uxQueueMessagesWaiting(graphicUpdateQueue) ||  (xQueueReceive(graphicUpdateQueue, &gp_read, 100/portTICK_PERIOD_MS) == pdFALSE))
+
+  for (;;)
   {
-    return;
-  }
+    if (!uxQueueMessagesWaiting(graphicUpdateQueue) || (xQueueReceive(graphicUpdateQueue, &gp_read, 100 / portTICK_PERIOD_MS) == pdFALSE))
+    {
+#ifdef SERIAL_DEBUG
+      Serial.println("#1::No messages in the queue yielding\n");
+#endif
+      taskYIELD();
+    }
+#ifdef SERIAL_DEBUG
+      Serial.println("#1::New Message Detected\n");
+#endif
 
 #ifdef SERIAL_DEBUG
-  Serial.println(String(gp_read.x_pos));
-  Serial.println(String(gp_read.y_pos));
+    Serial.println("#1::" + String(gp_read.x_pos));
+    Serial.println("#1::" + String(gp_read.y_pos));
 #endif
-  String toDisp = "LOVE YOU, BANGARAM! <3::" + String(gp_read.x_pos) + ":" + String(gp_read.y_pos);
-  spr.drawString(toDisp, 0, 0, 1);
-  spr.fillSprite(TFT_BLACK);
-  spr.setTextColor(TFT_GREEN);
-  spr.pushSprite(0, 0);
+    String toDisp = "LOVE YOU, BANGARAM! <3::" + String(gp_read.x_pos) + ":" + String(gp_read.y_pos);
+    spr.drawString(toDisp, 0, 0);
+    spr.pushSprite(0, 0);
+  }
 #ifdef SERIAL_DEBUG
-  Serial.println("Exiting TFT_write task \n");
+  Serial.println("#1::Exiting TFT_write task \n");
 #endif
 }
 
@@ -55,83 +51,68 @@ void gamepad_read(void *pvParameters)
 {
   // Read and parse the current input of the game pad
 #ifdef SERIAL_DEBUG
-  Serial.println("Entering gamepad_read task \n");
+  Serial.println("#2::Entering gamepad_read task \n");
 #endif
 
   gamepad_input current_input;
-  uint16_t x = X_REF - ss.analogRead(14);
-  uint16_t y = Y_REF - ss.analogRead(15);
-
-  if ((abs(x - last_input.x_pos) > 3) || (abs(y - last_input.y_pos) > 3))
+  for (;;)
   {
-#ifdef SERIAL_DEBUG
-    Serial.print("x: ");
-    Serial.print(x);
-    Serial.print(", ");
-    Serial.print("y: ");
-    Serial.println(y);
-#endif
-    last_input.x_pos = x;
-    last_input.y_pos = y;
+    bool isNew = false;
+    int16_t x = X_REF - ss.analogRead(14);
+    int16_t y = Y_REF - ss.analogRead(15);
+
+    if ((abs(x - last_input.x_pos) > 3) || (abs(y - last_input.y_pos) > 3))
+    {
+      last_input.x_pos = x;
+      last_input.y_pos = y;
+      isNew = true;
+    }
+
+    current_input.x_pos = last_input.x_pos;
+    current_input.y_pos = last_input.y_pos;
+
+    if (digitalRead(IRQ_PIN))
+    {
+      uint32_t buttons = ss.digitalReadBulk(BUTTON_MASK);
+
+      if (!(buttons & (1UL << BUTTON_A)))
+      {
+        current_input.button_A = true;
+      }
+      if (!(buttons & (1UL << BUTTON_B)))
+      {
+        current_input.button_B = true;
+      }
+      if (!(buttons & (1UL << BUTTON_Y)))
+      {
+        current_input.button_Y = true;
+      }
+      if (!(buttons & (1UL << BUTTON_X)))
+      {
+        current_input.button_X = true;
+      }
+      if (!(buttons & (1UL << BUTTON_SELECT)))
+      {
+        current_input.button_SEL = true;
+      }
+      if (!(buttons & (1UL << BUTTON_START)))
+      {
+        current_input.button_SRT = true;
+      }
+    }
+    
+    if (last_input == current_input)
+    {
+    #ifdef SERIAL_DEBUG
+      Serial.println("#2::New input detected\n");
+      Serial.println(prettify(current_input));
+    #endif      
+      xQueueSend(graphicUpdateQueue, &current_input, 100 / portTICK_RATE_MS);
+    }
+    vTaskDelay(200 / portTICK_RATE_MS);
   }
-
-  current_input.x_pos = last_input.x_pos;
-  current_input.y_pos = last_input.y_pos;
-
-  if (digitalRead(IRQ_PIN))
-  {
-    uint32_t buttons = ss.digitalReadBulk(BUTTON_MASK);
-
 #ifdef SERIAL_DEBUG
-    Serial.println(buttons, BIN);
-#endif
-
-    if (!(buttons & (1UL << BUTTON_A)))
-    {
-      current_input.button_A = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button A pressed");
-#endif
-    }
-    if (!(buttons & (1UL << BUTTON_B)))
-    {
-      current_input.button_B = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button B pressed");
-#endif
-    }
-    if (!(buttons & (1UL << BUTTON_Y)))
-    {
-      current_input.button_Y = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button Y pressed");
-#endif
-    }
-    if (!(buttons & (1UL << BUTTON_X)))
-    {
-      current_input.button_X = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button X pressed");
-#endif
-    }
-    if (!(buttons & (1UL << BUTTON_SELECT)))
-    {
-      current_input.button_SEL = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button SELECT pressed");
-#endif
-    }
-    if (!(buttons & (1UL << BUTTON_START)))
-    {
-      current_input.button_SRT = true;
-#ifdef SERIAL_DEBUG
-      Serial.println("\n Button START pressed");
-#endif
-    }
-  }
-  xQueueSend(graphicUpdateQueue, &current_input, 100/portTICK_RATE_MS);
-#ifdef SERIAL_DEBUG
-  Serial.println("Exiting gamepad_read task \n");
+  Serial.println("#2::Exiting gamepad_read task \n");
 #endif
 }
 
@@ -139,29 +120,34 @@ void main_init()
 {
 #ifdef SERIAL_DEBUG
   Serial.begin(115200);
-  while (!Serial.available()){}
-  Serial.println("\n==Setup Start");
+  while (!Serial.available())
+  {
+  }
+  Serial.println("MAIN::==Setup Start");
 #endif
   // for blink
   pinMode(LED_BUILTIN, OUTPUT);
 
 // tft
 #ifdef SERIAL_DEBUG
-  Serial.println("\n===TFT Init Start");
+  Serial.println("MAIN::===TFT Init Start");
 #endif
   tft.init();
   tft.setRotation(0);
-  spr.setTextDatum(TL_DATUM);
-  spr.createSprite(240, 180);
   tft.setTextWrap(true, false); // Wrap in x but not y
   tft.fillScreen(TFT_BLACK);
+  spr.setTextDatum(TL_DATUM);
+  spr.createSprite(240, 180);
+  spr.fillSprite(TFT_BLACK);
+  spr.setTextColor(TFT_GREEN);  
+  
 #ifdef SERIAL_DEBUG
-  Serial.println("\n===TFT Init Done");
+  Serial.println("MAIN::===TFT Init Done");
 #endif
 
 // Seesaw init
 #ifdef SERIAL_DEBUG
-  Serial.println("\n===Seesaw Init Start");
+  Serial.println("MAIN::===Seesaw Init Start");
 #endif
 
   ss.begin(0x50);
@@ -173,14 +159,14 @@ void main_init()
   pinMode(IRQ_PIN, INPUT);
 
 #ifdef SERIAL_DEBUG
-  Serial.println("\n===Seesaw Init Done");
+  Serial.println("MAIN::===Seesaw Init Done");
 #endif
 }
 
 void setup()
 {
   main_init();
-  graphicUpdateQueue = xQueueCreate(8, sizeof(gamepad_input));
+  graphicUpdateQueue = xQueueCreate(20, sizeof(gamepad_input));
   xTaskCreate(gamepad_read,
               "GamePad Read",
               configMINIMAL_STACK_SIZE,
@@ -201,7 +187,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
 #ifdef SERIAL_DEBUG
-  Serial.println("Hello!");
+  Serial.println("LOOP::Hello!");
 #endif
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
